0558da0fe1e065b070282b62c655d20a5c12a7f0
Nathan Jean <nathanjean@kyrosagency.com>
2021-05-21 11:57:48 -0400

    improve some styles

--- meteor-app/imports/ui/App.tsx
+++ meteor-app/imports/ui/App.tsx
@@ -4,9 +4,7 @@ import { UserProvider } from '../api/contexts/userContext'
 export const App = (props) => {
 	return (
 		<UserProvider>
-			<div className='App'>
-				{props.children}
-			</div>
+			{props.children}
 		</UserProvider>
 	)
 }
