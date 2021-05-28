70d6440e0c0c30540965ee5e33c1ebfd7d0ae041
kyros <nathanjean@kyrosagency.com>
2021-05-28 14:11:31 -0400

    fix seader

--- meteor-app/server/api/v1/seeders/seeder.js
+++ meteor-app/server/api/v1/seeders/seeder.js
@@ -40,7 +40,7 @@ export const seedUserData = () => {
 						first_name: first_name,
 						last_name: last_name,
 						account_id: account_id,
-						tags: ['manager'],
+						tags: [],
 						viewer_id: null
 					}
 				})
@@ -63,7 +63,7 @@ export const seedUserData = () => {
 			})
 		}
 
-		const createViewers = (account_id, viewer_id, username, email, first_name, last_name) => {
+		const createViewers = (account_id, viewer_id, tag, username, email, first_name, last_name) => {
 			return new Promise((resolve, reject) => {
 				const newUserId = Accounts.createUser({
 					username: username,
@@ -73,7 +73,7 @@ export const seedUserData = () => {
 						first_name: first_name,
 						last_name: last_name,
 						account_id: account_id,
-						tags: ['agent'],
+						tags: [tag],
 						viewer_id: viewer_id
 					}
 				})
@@ -108,50 +108,6 @@ export const seedUserData = () => {
 				} else {
 					reject('Failed to create Report_Data!!')
 				}
-
-				newObjectId = Report_Data.insert({
-					accountId: accountId,
-					collectionName: 'Transactions',
-					viewer_id: '3',
-					agentId: 'xxxyyyzzz',
-					price: 700.00
-				})
-
-				if (newObjectId) {
-					console.log(`Seeder - Report Data created: ${newObjectId}`)
-					resolve(newObjectId)
-				} else {
-					reject('Failed to create Viewer user!!')
-				}
-
-				const newObjectId2 = Report_Data.insert({
-					accountId: accountId,
-					collectionName: 'Transactions',
-					viewer_id: 'xxxyyyzzz',
-					agentId: '2',
-					price: 750.00
-				})
-
-				if (newObjectId2) {
-					console.log(`Seeder - Report Data created: ${newObjectId2}`)
-					resolve(newObjectId2)
-				} else {
-					reject('Failed to create Viewer user!!')
-				}
-
-				newObjectId2 = Report_Data.insert({
-					accountId: accountId,
-					collectionName: 'Transactions',
-					agentId: '2',
-					price: 1200.00
-				})
-
-				if (newObjectId2) {
-					console.log(`Seeder - Report Data created: ${newObjectId2}`)
-					resolve(newObjectId2)
-				} else {
-					reject('Failed to create Viewer user!!')
-				}
 			})
 		}
 
@@ -159,8 +115,8 @@ export const seedUserData = () => {
 		const run = async () => {
 			const account_id = await createAccount('City2Shore')
 			await createEditor(account_id, 'NathanJean', 'nathan@c2s.com', 'Nate', 'Jean')
-			await createViewers(account_id, 'xxxyyyzzz', 'CraigGeers', 'craig@c2s.com', 'Craig', 'Geers')
-			await createViewers(account_id, 'bbbcccaaa', 'ShelleyFrody', 'shelly@c2s.com', 'Shelley', 'Frody')
+			await createViewers(account_id, 'xxxyyyzzz', 'agent', 'CraigGeers', 'craig@c2s.com', 'Craig', 'Geers')
+			await createViewers(account_id, 'bbbcccaaa', 'manager', 'ShelleyFrody', 'shelly@c2s.com', 'Shelley', 'Frody')
 			await createReportData({ account_id, collection_name: 'Transactions', price: 500, viewer_id: 'xxxyyyzzz', date: new Date(2021, 03, 02), commission: .5 })
 			await createReportData({ account_id, collection_name: 'Transactions', price: 100, viewer_id: 'xxxyyyzzz', date: new Date(2021, 03, 01), commission: .1 })
 			await createReportData({ account_id, collection_name: 'Transactions', price: 200, viewer_id: 'bbbcccaaa', date: new Date(2021, 04, 01), commission: .25 })
