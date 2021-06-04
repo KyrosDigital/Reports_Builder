71260e99bcfcb428ebc61fd7f5627b2a8daef5eb
bwozniak <bwozniak@umich.edu>
2021-06-04 14:47:16 -0400

    changed clear button to button component

--- meteor-app/imports/ui/components/searchbar.tsx
+++ meteor-app/imports/ui/components/searchbar.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
 import { Input } from './inputs'
+import { Button } from './buttons'
 
 const SearchBar = ({ query, setQuery }) => {
-    const handleXClick = () => {
+    const handleClick = () => {
         setQuery("")
     }
 
@@ -11,11 +12,11 @@ const SearchBar = ({ query, setQuery }) => {
     return (
         <div className="flex relative search-bar-div items-stretch">
 					<Input placeholder={'type to search'} value={query} onChange={(e) => setQuery(e.target.value)} />
-					<button onClick={handleXClick}
-							className="bg-red-400 rounded border-black border-2 px-2 mx-2"
-					>
-							Clear
-					</button>
+					<Button 
+					color="red"
+					text="Clear"
+					onClick={() => handleClick()}
+					/>
         </div>
     );
 }
